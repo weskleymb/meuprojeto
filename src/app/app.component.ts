bf3b600e63637703f018066d5a4d202c75054b38
Weskley Bezerra <weskleymb@hotmail.com>
2019-10-01 21:07:02 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,18 @@ export class AppComponent {
 
   nome = 'Weskley';
 
+  adiciona() {
+    const numero = Math.round(Math.random() * 100);
+    this.nome = `Jose ${numero}`;
+    console.log(`Adicionado ${this.nome}`);
+  }
+
+  alteraNome(evento) {
+    this.nome = evento.target.value;
+  }
+
+  isDisabled(): boolean {
+    return this.nome.length <= 2;
+  }
+
 }
